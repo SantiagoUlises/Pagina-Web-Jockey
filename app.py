37c64620dc48023faf52587a70ba28e0cca88e5d
SantiagoUlises <ulisantherrera@gmail.com>
2024-06-29 13:09:31 -0300

    pull 5

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for,redirect
 app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+@app.route('/Directivos')
+def directivos():
+    return render_template(url_for('Directivos.html'))
+
+@app.route('/Contacto')
+def contacto():
+    return render_template(url_for('Contacto.html'))
+
+@app.route('/Galerias')
+def galerias():
+    return render_template(url_for('Galerias.html'))
+
+@app.route('/Agenda')
+def agenda():
+    return render_template(url_for('Agenda.html'))
+
+@app.route('/login')
+def login():
+    return render_template(url_for('login.html'))
+
 if __name__=='__main__':
     app.run(debug=True)
