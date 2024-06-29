3848f8dfeaf6b30cd920909ad21327bb8bea4739
SantiagoUlises <ulisantherrera@gmail.com>
2024-06-29 16:35:02 -0300

    pull 6

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
-from flask import Flask,render_template, url_for,redirect
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
+
 @app.route('/')
 def inicio():
     return render_template('index.html')
 
 @app.route('/Directivos')
 def directivos():
-    return render_template(url_for('Directivos.html'))
+    return render_template('Directivos.html')
 
 @app.route('/Contacto')
 def contacto():
-    return render_template(url_for('Contacto.html'))
+    return render_template('Contacto.html')
 
 @app.route('/Galerias')
 def galerias():
-    return render_template(url_for('Galerias.html'))
+    return render_template('Galerias.html')
 
 @app.route('/Agenda')
 def agenda():
-    return render_template(url_for('Agenda.html'))
+    return render_template('Agenda.html')
 
 @app.route('/login')
 def login():
-    return render_template(url_for('login.html'))
+    return render_template('login.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
