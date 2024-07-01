5af8e9daddec920770cebb18984c0cf6fe8c7adb
SantiagoUlises <ulisantherrera@gmail.com>
2024-07-01 12:49:14 -0300

    pull 6

--- static/js/funcionDate.js
+++ static/js/funcionDate.js
@@ -1,24 +1,19 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     function updateDate() {
-//         const dateElement = document.querySelector('.fecha');
-//         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-//         const today = new Date().toLocaleDateString('es-ES', options);
-//         dateElement.textContent = today;
-//     }
-    
-//     updateDate();
-
-// });
-
 function mostrafechaActual(){
-    const fechaActual=new Date();
-    const dia=fechaActual.getDate();
-    const mes=fechaActual.getMonth()+1;
-    const year=fechaActual.getFullYear();
-    const fechaForm = dia + '/' + mes + '/' + year
-    document.getElementById("fecha").textContent=fechaForm
-}   
+    const fechaActual = new Date();
+    
+    const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    
+    const diaSemana = diasSemana[fechaActual.getDay()];
+    const dia = fechaActual.getDate();
+    const mes = meses[fechaActual.getMonth()];
+    const year = fechaActual.getFullYear();
+    
+    const fechaForm = `${diaSemana}, ${dia} de ${mes} de ${year}`;
+    
+    document.getElementById("fecha").textContent = fechaForm;
+}
 
-window.onload=function(){
-    mostrafechaActual()
+window.onload = function(){
+    mostrafechaActual();
 }
